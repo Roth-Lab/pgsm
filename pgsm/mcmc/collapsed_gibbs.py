d3f0b6e4abfc1996afb31ee506d719202d28967c
Andrew Roth <andrewjlroth@gmail.com>
2016-12-21 18:04:44 +0000

    speed up collapse gibbs

--- pgsm/mcmc/collapsed_gibbs.py
+++ pgsm/mcmc/collapsed_gibbs.py
@@ -11,6 +11,9 @@ class Table(object):
         self.customers = customers
         self.dish = dish
 
+    def __contains__(self, x):
+        return x in self.customers
+
 
 class CollapsedGibbsSampler(object):
 
@@ -36,28 +39,23 @@ class CollapsedGibbsSampler(object):
         idx = np.arange(num_data_points)
         np.random.shuffle(idx)
         for i in idx:
-            clustering, tables = self._prune(clustering, tables)
             x = data[i]
-            z = clustering[i]
+            z = self._find_table(i, tables)
             tables[z].customers.remove(i)
             tables[z].dish.decrement(x)
 
             log_p = []
             new_params = self.dist.create_params(x)
-            log_p.append(self.partition_prior.log_tau_1(1) + self.partition_prior.log_tau_2(1) +
+            log_p.append(self.partition_prior.log_tau_1_diff(len(tables)) + self.partition_prior.log_tau_2(1) +
                          self.dist.log_marginal_likelihood(new_params))
             for table in tables:
                 if table.dish.suff_stats.N == 0:
                     continue
-                table.dish.increment(x)
-                log_p_c = self.partition_prior.log_tau_2(table.dish.suff_stats.N) + \
-                    self.dist.log_marginal_likelihood(table.dish)
-                table.dish.decrement(x)
-                log_p_c -= self.partition_prior.log_tau_2(table.dish.suff_stats.N) + \
-                    self.dist.log_marginal_likelihood(table.dish)
+                log_p_c = self.partition_prior.log_tau_2_diff(table.dish.suff_stats.N) + \
+                    self.dist.log_marginal_likelihood_diff(x, table.dish)
                 log_p.append(log_p_c)
 
-            p = exp_normalize(log_p)
+            p, _ = exp_normalize(log_p)
             z = np.random.multinomial(1, p).argmax() - 1
             if z == -1:
                 tables.append(Table([i, ], new_params))
@@ -65,11 +63,17 @@ class CollapsedGibbsSampler(object):
                 tables[z].customers.append(i)
                 tables[z].dish.increment(x)
 
-        return clustering, tables
+            tables = [x for x in tables if x.dish.suff_stats.N > 0]
+
+        return self._prune(clustering, tables)
 
     def _prune(self, clustering, tables):
         tables = [x for x in tables if x.dish.suff_stats.N > 0]
         for z, table in enumerate(tables):
-            for i in table.customers:
-                clustering[i] = z
+            clustering[np.array(table.customers)] = z
         return clustering, tables
+
+    def _find_table(self, customer, tables):
+        for i, t in enumerate(tables):
+            if customer in t:
+                return i
