bugfix: normal space normalisation missing

pgsm/math_utils.py
@@ -16,7 +16,7 @@ def exp_normalize(log_p):
     log_p = np.array(log_p)
     log_norm = log_sum_exp(log_p)
     p = np.exp(log_p - log_norm)
-    p / p.sum()
+    p = p / p.sum()
     return p, log_norm
 
 