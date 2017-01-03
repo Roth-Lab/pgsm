7822f54701f001204675adb53c7355d45e581314
Andrew Roth <andrewjlroth@gmail.com>
2017-01-03 14:21:09 +0000

    readibility improvements for gibbs code

--- pgsm/mcmc/collapsed_gibbs.py
+++ pgsm/mcmc/collapsed_gibbs.py
@@ -12,12 +12,21 @@ from pgsm.math_utils import exp_normalize
 
 class Table(object):
 
-    def __init__(self, customers, dish):
-        self.customers = customers
+    def __init__(self, dish):
         self.dish = dish
 
-    def __contains__(self, x):
-        return x in self.customers
+        self.customers = set()
+
+    def __contains__(self, idx):
+        return idx in self.customers
+
+    def add_customer(self, idx, value):
+        self.customers.add(idx)
+        self.dish.increment(value)
+
+    def remove_customer(self, idx, value):
+        self.customers.remove(idx)
+        self.dish.decrement(value)
 
 
 class CollapsedGibbsSampler(object):
@@ -36,49 +45,45 @@ class CollapsedGibbsSampler(object):
         tables = []
         for z in np.unique(clustering):
             idx = np.argwhere(clustering == z).flatten()
-            tables.append(Table(list(idx), self.dist.create_params()))
+            dish = self.dist.create_params()
+            tables.append(Table(dish))
             for i in idx:
-                tables[-1].dish.increment(data[i])
+                tables[-1].add_customer(i, data[i])
         return tables
 
     def _resample_params(self, clustering, data, tables):
         num_data_points = data.shape[0]
         idx = np.arange(num_data_points)
         np.random.shuffle(idx)
-        for i in idx:
-            x = data[i]
-            z = self._find_table(i, tables)
-            tables[z].customers.remove(i)
-            tables[z].dish.decrement(x)
-
-            log_p = []
+        for customer_idx in idx:
+            customer_data_point = data[customer_idx]
+            table_idx = self._find_table(customer_idx, tables)
+            tables[table_idx].remove_customer(customer_idx, customer_data_point)
+            log_p = np.zeros(len(tables) + 1, dtype=np.float64)
             new_params = self.dist.create_params()
-            new_params.increment(x)
-            log_p.append(self.partition_prior.log_tau_1_diff(len(tables)) + self.partition_prior.log_tau_2(1) +
-                         self.dist.log_marginal_likelihood(new_params))
-            for table in tables:
+            new_params.increment(customer_data_point)
+            log_p[-1] = self.partition_prior.log_tau_1_diff(len(tables)) + self.partition_prior.log_tau_2(1) + \
+                self.dist.log_marginal_likelihood(new_params)
+            for c, table in enumerate(tables):
                 if table.dish.N == 0:
                     continue
-                log_p_c = self.partition_prior.log_tau_2_diff(table.dish.N) + \
-                    self.dist.log_marginal_likelihood_diff(x, table.dish)
-                log_p.append(log_p_c)
-
+                log_p[c] = self.partition_prior.log_tau_2_diff(table.dish.N) + \
+                    self.dist.log_marginal_likelihood_diff(customer_data_point, table.dish)
             p, _ = exp_normalize(log_p)
-            z = np.random.multinomial(1, p).argmax() - 1
-            if z == -1:
-                tables.append(Table([i, ], new_params))
-            else:
-                tables[z].customers.append(i)
-                tables[z].dish.increment(x)
-
-            tables = [x for x in tables if x.dish.N > 0]
+            new_table_idx = np.random.multinomial(1, p).argmax()
+            is_new_table = (new_table_idx == len(tables))
+            if is_new_table:
+                dish = self.dist.create_params()
+                tables.append(Table(dish))
+            tables[new_table_idx].add_customer(customer_idx, customer_data_point)
+            tables = [t for t in tables if t.dish.N > 0]
 
         return self._prune(clustering, tables)
 
     def _prune(self, clustering, tables):
         tables = [x for x in tables if x.dish.N > 0]
         for z, table in enumerate(tables):
-            clustering[np.array(table.customers)] = z
+            clustering[np.array(list(table.customers))] = z
         return clustering, tables
 
     def _find_table(self, customer, tables):
