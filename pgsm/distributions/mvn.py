1ec8b2bb99e410a2790ecdc75be728911b1e9a8a
Andrew Roth <andrewjlroth@gmail.com>
2017-01-10 07:27:03 +0000

    refactored mvn code. pushed numba to funcs instead of classes

--- pgsm/distributions/mvn.py
+++ pgsm/distributions/mvn.py
@@ -11,14 +11,6 @@ import numpy as np
 from pgsm.math_utils import cholesky_log_det, cholesky_update, log_gamma
 
 
-@numba.jitclass([
-    ('dim', numba.int64),
-    ('nu', numba.float64),
-    ('r', numba.float64),
-    ('u', numba.float64[:]),
-    ('S_chol', numba.float64[:, :]),
-    ('log_det_S', numba.float64),
-])
 class MultivariateNormalPriors(object):
 
     def __init__(self, dim):
@@ -39,13 +31,6 @@ class MultivariateNormalPriors(object):
         return np.dot(self.S_chol, np.conj(self.S_chol.T))
 
 
-@numba.jitclass([
-    ('nu', numba.float64),
-    ('r', numba.float64),
-    ('u', numba.float64[:]),
-    ('S_chol', numba.float64[:, :]),
-    ('N', numba.int64),
-])
 class MultivariateNormalParameters(object):
 
     def __init__(self, nu, r, u, S_chol, N):
@@ -71,41 +56,49 @@ class MultivariateNormalParameters(object):
         return MultivariateNormalParameters(self.nu, self.r, self.u.copy(), self.S_chol.copy(), self.N)
 
     def decrement(self, x):
-        self.S_chol = cholesky_update(self.S_chol, np.sqrt(self.r / (self.r - 1)) * (x - self.u), -1)
-
-        self.nu -= 1
-
-        self.r -= 1
-
-        self.u = ((self.r + 1) * self.u - x) / self.r
+        self.nu, self.r, self.u, self.S_chol = _decrement_params(x, self.nu, self.r, self.u, self.S_chol, inplace=True)
 
         self.N -= 1
 
     def increment(self, x):
-        self.nu += 1
-
-        self.r += 1
-
-        self.u = ((self.r - 1) * self.u + x) / self.r
+        self.nu, self.r, self.u, self.S_chol = _increment_params(x, self.nu, self.r, self.u, self.S_chol, inplace=True)
 
         self.N += 1
 
-        self.S_chol = cholesky_update(self.S_chol, np.sqrt(self.r / (self.r - 1)) * (x - self.u), 1)
+
+@numba.jit(nopython=True)
+def _decrement_params(data_point, nu, r, u, S_chol, inplace=False):
+    S_chol = cholesky_update(S_chol, np.sqrt(r / (r - 1)) * (data_point - u), -1, inplace=inplace)
+
+    nu -= 1
+
+    r -= 1
+
+    u = ((r + 1) * u - data_point) / r
+
+    return nu, r, u, S_chol
 
 
-priors_type = numba.deferred_type()
-priors_type.define(MultivariateNormalPriors.class_type.instance_type)
+@numba.jit(nopython=True)
+def _increment_params(data_point, nu, r, u, S_chol, inplace=False):
+    nu += 1
+
+    r += 1
+
+    u = ((r - 1) * u + data_point) / r
+
+    S_chol = cholesky_update(S_chol, np.sqrt(r / (r - 1)) * (data_point - u), 1, inplace=inplace)
+
+    return nu, r, u, S_chol
 
 
-@numba.jitclass([
-    ('priors', priors_type),
-])
 class MultivariateNormalDistribution(object):
 
-    def __init__(self, dim):  # , priors=None):
-        #         if priors is None:
-        #         priors = MultivariateNormalPriors(dim)
-        self.priors = MultivariateNormalPriors(dim)
+    def __init__(self, dim, priors=None):
+        if priors is None:
+            priors = MultivariateNormalPriors(dim)
+
+        self.priors = priors
 
     def create_params(self):
         return MultivariateNormalParameters(
@@ -113,23 +106,17 @@ class MultivariateNormalDistribution(object):
             self.priors.r,
             self.priors.u.copy(),
             self.priors.S_chol.copy(),
-            0)
+            0
+        )
 
     def create_params_from_data(self, X):
-        D = X.shape[1]
-
         N = X.shape[0]
 
         nu = self.priors.nu + N
 
         r = self.priors.r + N
 
-        X_sum = np.zeros(D)
-
-        for n in range(N):
-            X_sum += X[n]
-
-        u = (self.priors.r * self.priors.u + X_sum) / r
+        u = (self.priors.r * self.priors.u + np.sum(X, axis=0)) / r
 
         S = self.priors.S + \
             np.dot(X.T, X) + \
@@ -145,12 +132,10 @@ class MultivariateNormalDistribution(object):
 
         N = params.N
 
-        d = np.arange(1, D + 1)
-
-        return -0.5 * N * D * np.log(np.pi) + \
-            0.5 * D * (np.log(self.priors.r) - np.log(params.r)) + \
-            0.5 * (self.priors.nu * self.priors.log_det_S - params.nu * params.log_det_S) + \
-            np.sum(log_gamma(0.5 * (params.nu + 1 - d)) - log_gamma(0.5 * (self.priors.nu + 1 - d)))
+        return _log_niw_marginal(
+            D, N,
+            params.nu, params.r, params.S_chol,
+            self.priors.nu, self.priors.r, self.priors.S_chol)
 
     def log_predictive_likelihood(self, data_point, params):
         '''
@@ -167,25 +152,58 @@ class MultivariateNormalDistribution(object):
         ```
 
         '''
-        D = self.priors.dim
-
-        u = (params.r * params.u + data_point) / (params.r + 1)
-
-        diff = np.sqrt((params.r + 1) / params.r) * (data_point - u)
-
-        S_chol = cholesky_update(params.S_chol, diff, 1, inplace=False)
-
-        return -0.5 * D * np.log(np.pi) + \
-            0.5 * D * (np.log(params.r) - np.log(params.r + 1)) + \
-            0.5 * (params.nu * cholesky_log_det(params.S_chol) - (params.nu + 1) * cholesky_log_det(S_chol)) + \
-            log_gamma(0.5 * (params.nu + 1)) - log_gamma(0.5 * (params.nu + 1 - D))
+        return _log_predictive_likelihood(data_point, params.nu, params.r, params.u, params.S_chol)
 
     def log_predictive_likelihood_bulk(self, data, params):
-        N = data.shape[0]
+        return _log_predictive_likelihood_bulk(data, params.nu, params.r, params.u, params.S_chol)
 
-        result = np.zeros(N, dtype=np.float64)
+    def log_pairwise_marginals(self, data, params):
+        return _log_pairwise_marginals(data, params.nu, params.r, params.u, params.S_chol)
 
-        for n in range(N):
-            result[n] = self.log_predictive_likelihood(data[n], params)
 
-        return result
+@numba.jit(nopython=True)
+def _log_pairwise_marginals(data, nu, r, u, S_chol):
+    N = data.shape[0]
+
+    log_p = np.zeros((N, N))
+
+    for i in range(N):
+        nu_new, r_new, u_new, S_chol_new = _increment_params(data[i], nu, r, u, S_chol, 1)
+
+        for j in range(i + 1):
+            log_p[i, j] = _log_predictive_likelihood(data[j], nu_new, r_new, u_new, S_chol_new)
+
+            log_p[j, i] = log_p[i, j]
+
+    return log_p
+
+
+@numba.jit(nopython=True)
+def _log_predictive_likelihood_bulk(data, nu, r, u, S_chol):
+    N = data.shape[0]
+
+    result = np.zeros(N, dtype=np.float64)
+
+    for n in range(N):
+        result[n] = _log_predictive_likelihood(data[n], nu, r, u, S_chol)
+
+    return result
+
+
+@numba.jit(nopython=True)
+def _log_predictive_likelihood(data_point, nu, r, u, S_chol):
+    D = len(data_point)
+
+    nu_new, r_new, _, S_chol_new = _increment_params(data_point, nu, r, u, S_chol, inplace=False)
+
+    return _log_niw_marginal(D, 1, nu_new, r_new, S_chol_new, nu, r, S_chol)
+
+
+@numba.jit(nopython=True)
+def _log_niw_marginal(D, N, nu_new, r_new, S_chol_new, nu_old, r_old, S_chol_old):
+    d = np.arange(1, D + 1)
+
+    return -0.5 * N * D * np.log(np.pi) + \
+        0.5 * D * (np.log(r_old) - np.log(r_new)) + \
+        0.5 * (nu_old * cholesky_log_det(S_chol_old) - nu_new * cholesky_log_det(S_chol_new)) + \
+        np.sum(log_gamma(0.5 * (nu_new + 1 - d)) - log_gamma(0.5 * (nu_old + 1 - d)))
